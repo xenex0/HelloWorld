03a5b77d59692a3f12e92715b0d85eed75ee82e5
xenex0 <tarjafan09@gmail.com>
2022-06-19 23:44:53 +0300

    main

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var toggleButton: UIButton!
+    @IBOutlet var helloWorldLabal: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        helloWorldLabal.isHidden = true
+        toggleButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeButton(_ sender: Any) {
+        if helloWorldLabal.isHidden {
+            helloWorldLabal.isHidden = false
+            toggleButton.setTitle("Hide text", for: .normal)
+        } else {
+            helloWorldLabal.isHidden = true
+            toggleButton.setTitle("Show text", for: .normal)
+        }
+    }
+    
 }
 
